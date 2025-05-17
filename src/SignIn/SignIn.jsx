25ea87c508d25bd36b6958040a9248536247df37
sarfarazakram576 <sarfaraz.akram055@gmail.com>
2025-05-17 23:02:54 +0600

    make too many things

--- src/SignIn/SignIn.jsx
+++ src/SignIn/SignIn.jsx
@@ -2,9 +2,10 @@ import React, { useContext, useState } from "react";
 import { Link } from "react-router";
 import { AuthContext } from "../Authentication/AuthContext";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
+import Swal from "sweetalert2";
 
 const SignIn = () => {
-      const [showPassword, setShowPassword] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
   const { signInUser } = useContext(AuthContext);
 
   const handleSignIn = (e) => {
@@ -15,7 +16,33 @@ const SignIn = () => {
     const password = formData.get("password");
 
     signInUser(email, password)
-      .then(() => {})
+      .then((result) => {
+        Swal.fire({
+          position: "center",
+          icon: "success",
+          title: "<h1>Congratulations!</h1>",
+          text: "Yo successfully logged In",
+          showConfirmButton: false,
+          timer: 1200,
+        });
+
+        const signInInfo = {
+          email,
+          lastSignInTime: result.user.metadata.lastSignInTime,
+        };
+
+        fetch('http://localhost:3000/users',{
+            method: "PATCH",
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(signInInfo)
+        })
+         .then(res=>res.json())
+         .then(data=> {
+            console.log(data)
+         })
+      })
       .catch((error) => console.dir(error));
 
     form.reset();
@@ -29,32 +56,32 @@ const SignIn = () => {
             <label className="label">Email</label>
             <input
               type="email"
-              className="input mb-4"
+              className="input mb-4 text-lg placeholder:text-[15px] placeholder:font-bold rancho"
               name="email"
               placeholder="Enter your email"
             />
             <label className="label">Password</label>
-          <div className="relative">
-            <input
-              type={showPassword ? "text" : "password"}
-              className="input placeholder:font-bold rancho"
-              name="password"
-              placeholder="Enter your password"
-            />
-            {showPassword ? (
-              <FaEyeSlash
-                onClick={() => setShowPassword(!showPassword)}
-                className="absolute top-3 right-6 cursor-pointer z-10"
-                size={17}
+            <div className="relative">
+              <input
+                type={showPassword ? "text" : "password"}
+                className="input text-lg placeholder:text-[15px] placeholder:font-bold rancho"
+                name="password"
+                placeholder="Enter your password"
               />
-            ) : (
-              <FaEye
-                onClick={() => setShowPassword(!showPassword)}
-                className="absolute top-3 right-6 cursor-pointer z-10"
-                size={17}
-              />
-            )}
-          </div>
+              {showPassword ? (
+                <FaEyeSlash
+                  onClick={() => setShowPassword(!showPassword)}
+                  className="absolute top-3 right-6 cursor-pointer z-10"
+                  size={17}
+                />
+              ) : (
+                <FaEye
+                  onClick={() => setShowPassword(!showPassword)}
+                  className="absolute top-3 right-6 cursor-pointer z-10"
+                  size={17}
+                />
+              )}
+            </div>
             <div>
               <a className="link link-hover">Forgot password?</a>
             </div>
